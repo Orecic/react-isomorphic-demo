071658571f99940ffb6f88caadb7cf2e79eb511a
Mingbing Chen <mingbingchen@Mingbings-MacBook-Pro.local>
2017-07-20 17:36:29 +0800

    init

--- pre-commit.tsx
+++ pre-commit.tsx
@@ -1,8 +1,8 @@
-var exec = require('child_process').exec;
-var fs = require('fs');
-var errTip = ['还存在很多错误的地方哦！，避免隐患，还是现在改了吧！', '哎呀呀！还有错误哦！'];
-var successTip = ['不错哦！加油！', '赞！', '棒棒哒！'];
-var lint = function(cb) {
+let exec = require('child_process').exec;
+let fs = require('fs');
+let errTip = ['还存在很多错误的地方哦！，避免隐患，还是现在改了吧！', '哎呀呀！还有错误哦！'];
+let successTip = ['不错哦！加油！', '赞！', '棒棒哒！'];
+let lint = function(cb) {
     exec('tslint --project tsconfig.json --format stylish', function(error, stdout, stderr) {
         if(stdout) {
             console.log('\x1B[31m%s',errTip[Math.floor(errTip.length*Math.random())]);
@@ -14,15 +14,15 @@ var lint = function(cb) {
     });
 }
 
-var taskList = [lint];
+let taskList = [lint];
 
-var task = function() {
+let task = function() {
     if(!taskList.length) {
         console.log('\x1B[32m%s', successTip[Math.floor(successTip.length*Math.random())]);
         process.exit(0);
         return;
     }
-    var func = taskList.shift();
+    let func = taskList.shift();
     func(function(pass) {
         if(pass === 1) {
             process.exit(1);
@@ -32,7 +32,7 @@ var task = function() {
     });
 }
 
-var startTask = function() {
+let startTask = function() {
     console.log('开始检查代码咯！O(∩_∩)O~\n');
     task();
 }
