e54a61e99aed2c8aef6660eca46b0f98d4e01ac0
Mingbing Chen <mingbingchen@Mingbings-MacBook-Pro.local>
2017-07-20 16:37:55 +0800

    Init

--- pre-commit.tsx
+++ pre-commit.tsx
@@ -5,18 +5,27 @@
  * pre commit 检查
  */
 /* tslint-disable */
+
 const exec = require('child_process').exec;
 const log = require('./log');
+
 const fs = require('fs');
-let eslintConfig = fs.readFileSync('./tslint.json', 'utf-8');
+
+let eslintConfig = fs.readFileSync('./tslint.json','utf-8');
+
 const globals = [];
+
 let pass = 0;
+
 const ext = ['tsx'];
+
 log.info('Linting code, please waiting...');
+
 exec('git diff-index --cached --name-status HEAD', (error, stdout, stderr) => {
-    if (stdout.length) {
+    if(stdout.length) {
         const array = stdout.split('\n');
         const files = [];
+
         array.pop();
         array.forEach(item => {
             const expr = /[\D]\s*(.+)/.exec(item);
@@ -28,27 +37,32 @@ exec('git diff-index --cached --name-status HEAD', (error, stdout, stderr) => {
                 }
             }
         });
+
         try {
             const report = cli.executeOnFiles(files);
+
             report.results.forEach(item => {
                 if (item.errorCount > 0) {
                     const messages = item.messages;
+
                     log.info(`${item.filePath}`);
                     messages.forEach(msg => {
                         log.error(`line ${msg.line}, col ${msg.column}, message: ${msg.message}, rule: ${msg.ruleId}, type: ${msg.nodeType}`);
                     });
+
                     pass = 1;
                 }
             });
-        }
-        catch (ex) {
+        } catch (ex) {
             log.warn(ex);
         }
-        if (pass === 0) {
+
+        if(pass === 0){
             log.info(`SUCCESS: Everything is OK!`);
         }
         process.exit(pass);
     }
+
     if (error !== null) {
         log.error('exec error: ' + error);
     }
