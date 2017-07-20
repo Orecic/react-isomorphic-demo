fb63d1d2d3bcdb370cd7297e5289cb19ca675a8c
Mingbing Chen <mingbingchen@Mingbings-MacBook-Pro.local>
2017-07-20 16:39:45 +0800

    init

--- constants/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// 每页分页条数
-exports.PAGE_SIZE = 10;
-exports.INCREMENT_ENTHUSIASM = 'INCREMENT_ENTHUSIASM';
-exports.DECREMENT_ENTHUSIASM = 'DECREMENT_ENTHUSIASM';
