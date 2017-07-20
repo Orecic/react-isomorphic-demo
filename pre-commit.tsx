26e9f89dec246a6473194ee723df377dd09462ac
Mingbing Chen <mingbingchen@Mingbings-MacBook-Pro.local>
2017-07-20 17:28:15 +0800

    Init

--- pre-commit.tsx
+++ pre-commit.tsx
@@ -3,10 +3,10 @@ var fs = require('fs');
 var errTip = ['还存在很多错误的地方哦！，避免隐患，还是现在改了吧！', '哎呀呀！还有错误哦！'];
 var successTip = ['不错哦！加油！', '赞！', '棒棒哒！'];
 var lint = function(cb) {
-    exec('tslint --project tsconfig.json --format stylish', function(error, stdout, stderr) {// 通过node子进程执行命令
+    exec('tslint --project tsconfig.json --format stylish', function(error, stdout, stderr) {
         if(stdout) {
             console.log('\x1B[31m%s',errTip[Math.floor(errTip.length*Math.random())]);
-            console.log('\x1B[37m', stdout);//输出eslint错误信息
+            console.log('\x1B[37m', stdout);
             cb(1);
             return;
         }
@@ -15,7 +15,7 @@ var lint = function(cb) {
 }
 
 var taskList = [lint];
-// 执行检查
+
 var task = function() {
     if(!taskList.length) {
         console.log('\x1B[32m%s', successTip[Math.floor(successTip.length*Math.random())]);
@@ -37,5 +37,4 @@ var startTask = function() {
     task();
 }
 
-// 执行检查
 startTask();
